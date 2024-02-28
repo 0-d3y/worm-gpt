dc94cbfb8c84496ec8ddc5859e9449841ab389cb
SAMI DEV <103077384+mr-sami-x@users.noreply.github.com>
2024-02-28 14:10:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-# Telegram: @SaMi_ye 
-# Instagram: cyber_77k 
+# Telegram: @SaMi_ye
+# Instagram: cyber_77k
 
 from sami_ai import Worm_GPT
 import time
-import  platform
+import platform
 import os
+
 try:
-  import pyfiglet
-except:
-  os.system('pip install pyfiglet')
-  os.system("pip install --upgrade pyfiglet")
-  
-  
+    import pyfiglet
+except ImportError:
+    os.system('pip install pyfiglet')
+    os.system("pip install --upgrade pyfiglet")
+
 # Coded By Mr.SaMI
 red = "\033[1;31m"
 green = "\033[1;32m"
 cyan = "\033[1;36m"
 
 
-
-
 def banner():
     sami = pyfiglet.Figlet(font="slant")
-    banner = sami.renderText("WormGPT")
-    print(green+banner)
+    banner_text = sami.renderText("WormGPT")
+    print(green + banner_text)
 
 
 def clear():
@@ -31,30 +29,27 @@ def clear():
         os.system("cls")
     else:
         os.system("clear")
-        
-        
-def main(key,model):
+
+
+def main(key, model):
     banner()
 
     while True:
-        user_input = input(red+"YOU $_ "+green)
-        
+        user_input = input(red + "YOU $_ " + green)
+
         if user_input.lower() == 'exit':
             print("Exiting WormGPT. Goodbye!")
             break
-        if user_input.lower() == 'clear':
+        elif user_input.lower() == 'clear':
             clear()
-
-        
-
         else:
-        	result = Worm_GPT(user_input, key, model)
-        	print(red+"WormGPT : "+green+result["response"])
-        	print("\n")
+            result = Worm_GPT(user_input, key, model)
+            print(f"{red}WormGPT : {green}{result['response']}\n")
+
 
 if __name__ == "__main__":
     banner()
     key = "YOUR_KEY_OPEN_AI"
-    model ="gpt-3.5-turbo-16k-0613"
+    model = "gpt-3.5-turbo-16k-0613"
     clear()
-    main(key,model)
+    main(key, model)
